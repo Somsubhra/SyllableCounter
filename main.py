1996c75c8d1a0a7d96564f0b03d437f8637c244e
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-09-05 21:03:20 +0530

    Tree walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # All python imports
-from os import path
+from os import walk, path, stat, mkdir
 
 # All constant definitions
 INPUT_DIRECTORY = 'corpus'
@@ -16,8 +16,29 @@ class SyllableCounter:
 
 # The main method
 def main():
+
+  # Check for the input directory
+  try:
+    stat(INPUT_DIRECTORY)
+  except:
+    print "No files present in input directory " + INPUT_DIRECTORY + "..."
+    print "Terminating program..."
+    exit()
+
+  # Create the output directory
+  try:
+    stat(OUTPUT_DIRECTORY)
+  except:
+    mkdir(OUTPUT_DIRECTORY)
+
+  # Create instance of the syllable counter
   c = SyllableCounter()
-  c.count(INPUT_DIRECTORY, OUTPUT_DIRECTORY)
+
+  # Walk through the input directory and count number of syllables
+  for(dirPath, _, fileNames) in walk(INPUT_DIRECTORY):
+    for fileName in fileNames:
+      inputFile = path.join(dirPath, fileName)
+      c.count(inputFile, path.join(OUTPUT_DIRECTORY, dirPath, fileName))
 
 if __name__ == '__main__':
   main()
